a8fa2d20b274824e9d434bbb012f807b0bd7b35f
Ross Merriam <rossmerriam@gmail.com>
2024-01-11 15:43:03 -0400

    fixed bug in models definition.

--- seotools/serp_ranking/models.py
+++ seotools/serp_ranking/models.py
@@ -3,10 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class SerpResult(models):
+class SerpResult(models.Model):
     search_query = models.CharField(max_length=200)
     date = models.DateTimeField("query date")
     result = models.JSONField()
-
-    def __str__(self):
-        return self.search_query
